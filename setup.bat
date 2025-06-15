d73fabc9eb4b0ebec92e836c124e1ec019909675
Owen Prathama Binusian 2027 <145041024+owenp28@users.noreply.github.com>
2025-06-15 14:50:04 +0700

    Add files via upload

---
+++ setup.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Menginstall library yang diperlukan untuk aplikasi Bitcoin Price Prediction...
+echo.
+
+pip install -r requirements.txt
+
+echo.
+echo Instalasi selesai!
+echo Jalankan aplikasi dengan mengklik run_app.bat
+echo.
+pause
